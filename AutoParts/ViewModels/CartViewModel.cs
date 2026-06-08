c7d8acdd48a519be23cca0135886bf43f226a51f
D-a-sh-a <dariya2004tar@gmail.com>
2026-06-08 23:09:54 +0300

    Change Cart

---
+++ AutoParts/ViewModels/CartViewModel.cs
@@ -0,0 +1,11 @@
+﻿using AutoParts.Models;
+
+namespace AutoParts.ViewModels
+{
+	public class CartViewModel
+	{
+		public List<CartItem> Items { get; set; } = new List<CartItem>();
+
+		public decimal TotalSum => Items.Sum(item => item.AutoPart.Price * item.Quantity);
+	}
+}
